40b61987d820d32fbe23e0fa76b674817ec67902
fis007 <schaffhauserfilip@gmail.com>
2023-07-09 19:40:15 -0400

    added first db models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField() 
+
+class Menu(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(decimal_places=2, max_digits=10)
+    Inventory = models.IntegerField()
